6430f7f104e9638f5f356f2e62d42c643dbd55b6
Akash11144 <82667535+Akash11144@users.noreply.github.com>
2023-05-03 16:24:56 +0530

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,20 @@
+// to find anagram strings from array of strings
+const ARR_LENGTH = 10000;
+const findAnagrams = (arr) => {
+  if (arr.length > ARR_LENGTH) return "Array length should be less than 10000";
+  if (arr.length < 2) return [arr];
+  let anagrams = {};
+  arr.forEach((word) => {
+    const sortedWord = word.split("").sort().join("");
+    anagrams[sortedWord]
+      ? anagrams[sortedWord].push(word)
+      : (anagrams[sortedWord] = [word]);
+  });
+  return Object.values(anagrams).filter((anagram) => anagram.length > 0);
+};
+
+console.log(findAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(findAnagrams([""]));
+console.log(findAnagrams(["a"]));
+console.log(findAnagrams([]));
+console.log(findAnagrams());
